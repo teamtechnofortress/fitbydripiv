386fd4c24aee2c11c699611088bd81eb1411a9b1
teamtechnofortress <team.technofortress@gmail.com>
2026-02-24 16:29:57 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>FitByDrip Web Application</title>
   <link rel="stylesheet" type="text/css" href="/loader.css" />
-  <script type="module" crossorigin src="/assets/index.cb19f53e.js"></script>
-  <link rel="stylesheet" href="/assets/index.84c22974.css">
+  <script type="module" crossorigin src="/assets/index.e354b59d.js"></script>
+  <link rel="stylesheet" href="/assets/index.cc6ec66e.css">
 </head>
 
 <body>
   <div id="app">
     <div id="loading-bg">
-      <div class="loading-logo">
-        <img src="/uploads/logo_temp.png?<?= time()?>" alt="Logo" />
-      </div>
       <div class="loading">
         <div class="effect-1 effects"></div>
         <div class="effect-2 effects"></div>
@@ -24,16 +21,15 @@
       </div>
     </div>
   </div>
-  
+
   <script>
     const loaderColor = localStorage.getItem('Vuexy-initial-loader-bg') || '#FFFFFF'
-    const primaryColor = localStorage.getItem('Vuexy-initial-loader-color') || '#7367F0'
+    const primaryColor = localStorage.getItem('Vuexy-initial-loader-color') || '#059669'
 
     if (loaderColor)
       document.documentElement.style.setProperty('--initial-loader-bg', loaderColor)
     if (primaryColor)
       document.documentElement.style.setProperty('--initial-loader-color', primaryColor)
-
   </script>
 </body>
 
